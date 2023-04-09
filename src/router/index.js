31a8be830ec31c2dce818602ef2268a23193b18c
djmnb <1632782287@qq.com>
2023-04-09 12:51:12 +0800

    弄好路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,27 @@
-import { createRouter,createWebHashHistory } from 'vue-router';
+import Login from "../components/Login.vue"
+import Register from "../components/Register.vue"
+import ForgotPassword from "../components/ForgotPassword.vue"
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+
 const routes = [
-    
+    {
+        path: "/",
+        redirect: "/login"
+    },
+    {
+        path: "/login",
+        component: Login
+    },
+    {
+        path: "/register",
+        component: Register
+    },
+    {
+        path: "/forgot-password",
+        component: ForgotPassword
+    },
+
 ]
 
 export default createRouter({
