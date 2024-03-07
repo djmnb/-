cd932af50b3b697e5dac106f42fd793d5338b183
djmnb <1632782287@qq.com>
2024-03-07 22:43:00 +0800

    编写文档

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   devServer: {
+    port:8080,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
